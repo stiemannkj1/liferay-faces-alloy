bf5611bbdb7e34b26c97e8e8a237961c87127e39
pcwhite <philip.white@liferay.com>
2015-11-17 16:47:50 -0500

    completed use case tests for alloyInputTextGeneral & alloyInputTextConversion

--- demos/showcase/alloy-showcase-webapp/src/test/js/alloyinputTextTest.js
+++ demos/showcase/alloy-showcase-webapp/src/test/js/alloyinputTextTest.js
@@ -1,56 +1,68 @@
 // http://docs.casperjs.org/en/latest/testing.html#browser-tests
 // http://docs.casperjs.org/en/latest/modules/
 
-var url = "http://localhost:8080/alloy-showcase-webapp-3.0.0-SNAPSHOT/views/component.faces?componentPrefix=alloy&componentName=inputtext&componentUseCase=";
+var url = "http://localhost:8080/alloy-showcase-webapp-2.0.0-SNAPSHOT/views/component.faces?componentPrefix=alloy&componentName=inputtext&componentUseCase=";
 var useCase = 'general';
 var x = require('casper').selectXPath;
 
 casper.test.begin('Test alloy:inputText ' + useCase, function suite(test) {
 
+	var buttonXpath = "//button[text()='Submit']";
+	var buttonSelector = x(buttonXpath); 
+	var successXpath = "//div[@class='alloy-field control-group success']";
+	var successSelector = x(successXpath);
+
 	casper.start(url + useCase, function () {
 
-		var inputXpath = '//input[contains(@id,":text")]';
-		var buttonXpath = '//button[text()="Submit"]';
-		var modelValueXpath = '//span[contains(@id,":modelValue")]';
-		var checkboxXpath = '//input[@class="alloy-select-boolean-checkbox checkbox"]';
-		
+		this.test.assertExists(buttonSelector);
 
-		this.test.assertExists({
-			type: 'xpath',
-			path: inputXpath
-		}, 'input element exists');
+		this.click(buttonSelector);
+                this.waitUntilVisible(successSelector, function () {
+                        this.test.assertExists(successSelector);
+                });
+	});
+	
+	casper.then(function () {
 
-		this.test.assertExists({
-			type: 'xpath',
-			path: buttonXpath
-		}, 'submit button element exists');
-
-		this.test.assertExists({
-			type: 'xpath',
-			path: modelValueXpath
-		}, 'modelValue element exists');
-		
-		this.test.assertExists({
-			type: 'xpath',
-			path: checkboxXpath
-		}, 'checkbox element exists');
-
-		var checkboxSelector = x(checkboxXpath);
+		var checkboxXpath = "//input[@class='alloy-select-boolean-checkbox checkbox']";
+		var checkboxSelector = x(checkboxXpath); 
+                this.test.assertExists(checkboxSelector);
 		this.click(checkboxSelector);
-		this.click(x(buttonXpath));
-		var errorTextXpath = '//span[@class="alloy-message help-inline"]';
-		test.assertExists(x(errorTextXpath));
+		this.waitWhileVisible(successSelector, function () {
+			this.click(buttonSelector);
+		});
+        });
+
+        casper.then(function () {
+
+		var errorTextXpath = "//span[@class='alloy-message help-inline']";
+		var errorTextSelector = x(errorTextXpath);
+		this.waitUntilVisible(errorTextSelector, function () {
+			this.test.assertExists(errorTextSelector);
+		});
+        });
+
+        casper.then(function () {
+		
+		var inputXpath = "//input[contains(@id,':text')]";
+		var inputSelector = x(inputXpath);
+		this.test.assertExists(inputSelector);
 		
 		var magic = "Hello World!";
-		this.sendKeys(x(inputXpath), magic);
-		this.click(x(buttonXpath));
+		this.sendKeys(inputSelector, magic);
+		this.click(buttonSelector);
+
+                var modelValueXpath = "//span[contains(@id,':modelValue')]";
+                var modelValueSelector = x(modelValueXpath);
+                this.test.assertExists(modelValueSelector);
 		
-		var modelValueSelector = x(modelValueXpath);
 		this.waitForSelectorTextChange(modelValueSelector, function () {
 			var modelValueText = this.fetchText(modelValueSelector);
 			this.test.assertEquals(modelValueText, magic, "modelValueText equals \"" + magic + "\"");
 		});
-	}).run(function () {
+	});
+		
+	casper.run(function () {
 		test.done();
 	});
 });
@@ -59,36 +71,80 @@ useCase = 'conversion';
 
 casper.test.begin('Test alloy:inputText ' + useCase, function suite(test) {
 
-	casper.start(url + useCase, function () {
+        var buttonXpath = "//button[text()='Submit']";
+        var buttonSelector = x(buttonXpath);
+        var inputXpath = "//input[contains(@id,':text')]";
+        var inputSelector = x(inputXpath);
 
-		var inputXpath = '//input[contains(@id,":text")]';
-		var buttonXpath = '//button[text()="Submit"]';
-		var modelValueXpath = '//span[contains(@id,":modelValue")]';
+        casper.start(url + useCase, function () {
 
-		this.test.assertExists({
-			type: 'xpath',
-			path: inputXpath
-		}, 'input element exists');
+                this.test.assertExists(buttonSelector);
+                this.test.assertExists(inputSelector);
+                var magic = "april 03 0033";
+                this.sendKeys(inputSelector, magic, {reset: true});
+                this.click(buttonSelector);
 
-		this.test.assertExists({
-			type: 'xpath',
-			path: buttonXpath
-		}, 'submit button element exists');
+                var errorTextXpath = "//span[@class='alloy-message help-inline']";
+                var errorTextSelector = x(errorTextXpath);
+                this.waitUntilVisible(errorTextSelector, function () {
+                        this.test.assertExists(errorTextSelector);
+                });
+        });
 
-		this.test.assertExists({
-			type: 'xpath',
-			path: modelValueXpath
-		}, 'modelValue element exists');
+        casper.then(function () {
 
-		var magic = "Hello World!";
-		this.sendKeys(x(inputXpath), magic);
-		this.click(x(buttonXpath));
-		var modelValueSelector = x(modelValueXpath);
-		this.waitForSelectorTextChange(modelValueSelector, function () {
-			var modelValueText = this.fetchText(modelValueSelector);
-			this.test.assertEquals(modelValueText, magic, "modelValueText equals \"" + magic + "\"");
-		});
-	}).run(function () {
-		test.done();
-	});
+                var magicIn = "april 03, 0033";
+                var magicOut = "Apr 3, 0033";
+                this.sendKeys(inputSelector, magicIn, {reset: true});
+                this.click(buttonSelector);
+
+                var modelValueXpath = "(//span[contains(@id,':modelValue')])[1]";
+                var modelValueSelector = x(modelValueXpath);
+                this.test.assertExists(modelValueSelector);
+
+                this.waitForSelectorTextChange(modelValueSelector, function () {
+                        var modelValueText = this.fetchText(modelValueSelector);
+                        this.test.assertEquals(modelValueText, magicOut, "modelValueText equals \"" + magicOut + "\"");
+                });
+        });
+
+	var buttonXpath2 = "(//button[text()='Submit'])[2]";
+	var buttonSelector2 = x(buttonXpath2);
+        var inputXpath2 = "(//input[contains(@id,':text')])[2]";
+        var inputSelector2 = x(inputXpath2);
+
+        casper.then(function () {
+
+                this.test.assertExists(buttonSelector2);
+                this.test.assertExists(inputSelector2);
+                var magic = "04/03033";
+                this.sendKeys(inputSelector2, magic, {reset: true});
+                this.click(buttonSelector2);
+
+                var errorTextXpath = "//span[@class='alloy-message help-inline']";
+                var errorTextSelector = x(errorTextXpath);
+                this.waitUntilVisible(errorTextSelector, function () {
+                        this.test.assertExists(errorTextSelector);
+                });
+        });
+
+        casper.then(function () {
+
+                var magicIn = "4/3/33";
+                var magicOut = "04/03/0033";
+                this.sendKeys(inputSelector2, magicIn, {reset: true});
+                this.click(buttonSelector2);
+
+                var modelValueXpath2 = "(//span[contains(@id,':modelValue')])[2]";
+                var modelValueSelector = x(modelValueXpath2);
+                this.test.assertExists(modelValueSelector);
+
+                this.waitForSelectorTextChange(modelValueSelector, function () {
+                        var modelValueText = this.fetchText(modelValueSelector);
+                        this.test.assertEquals(modelValueText, magicOut, "modelValueTextRight equals \"" + magicOut + "\"");
+                });
+        });
+        casper.run(function () {
+                test.done();
+        });
 });
